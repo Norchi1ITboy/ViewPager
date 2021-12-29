f0409cc7b31707053e5912faf5e5fad894f0e257
DESKTOP-35CDSQF\User <nikaberianidze01@gmail.com>
2021-12-29 15:19:58 +0400

    davaleba

---
+++ app/src/main/java/com/example/davaleba_4_nikaberianidze/ViewPager.kt
@@ -0,0 +1,13 @@
+package com.example.davaleba_4_nikaberianidze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPager : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager)
+
+
+    }
+}
